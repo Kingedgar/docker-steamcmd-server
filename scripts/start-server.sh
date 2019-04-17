8374422cb667e7208667e5b88332cfe0c7a651c2
ich777 <christophhummer@gmail.com>
2019-04-17 13:49:22 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$GAME_NAME" == "tf" ] ; then 
     echo "---Fetching required files for TeamFortress2---"
-    dpkg --add-architecture i386
-    apt-get update
-    apt-get -y install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386
+    sudo dpkg --add-architecture i386
+    sudo apt-get update
+    sudo apt-get -y install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386
 fi
 
 echo "---Before---"
