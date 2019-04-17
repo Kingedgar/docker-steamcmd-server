36b3bcddbf4b3655a4a260da6e32be15bcb728d9
ich777 <christophhummer@gmail.com>
2019-04-17 15:54:42 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+usermod -o -u "$UID" steam
+groupmod -o -g "$GID" steam
+
+chown abc:abc ${SERVER_DIR}
+
+if [ "$GAME_NAME" == "tf" ] ; then 
+    echo "---Fetching required files for TeamFortress2---"
+    dpkg --add-architecture i386
+    apt-get update
+    apt-get -y install lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc1 libtinfo5:i386 libcurl3-gnutls:i386
+fi
+
+
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     echo "Steamcmd not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
-    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
-    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+    steam tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+    steam rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
 echo "---Update SteamCMD---"
