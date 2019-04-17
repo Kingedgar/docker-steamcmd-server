90986a9ce6f23452c30bd6353e8d17e964eaf47a
ich777 <christophhummer@gmail.com>
2019-04-17 14:31:26 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,21 +6,24 @@ if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
-echo "---Update steamcmd---"
+echo "---Update SteamCMD---"
 ${STEAMCMD_DIR}/steamcmd.sh \
     +login anonymous \
     +quit
+echo "---Update SteamCMD finished---"
     
-echo "---Update server---"
+echo "---Update Server---"
 ${STEAMCMD_DIR}/steamcmd.sh \
     +login anonymous \
     +force_install_dir $SERVER_DIR \
     +app_update $GAME_ID \
     +quit
+echo "---Update Server finished---"
 
 echo "---Prepare Server---"
 mkdir ${DATA_DIR}/.steam/sdk32
 cp -R ${SERVER_DIR}/bin/* ${DATA_DIR}/.steam/sdk32/
+echo "---Preparation finished---"
    
 echo "---Start Server---"
 ${SERVER_DIR}/srcds_run -game $GAME_NAME $GAME_PARAMS -console +port $GAME_PORT
